ccb34d75595f69a3eae0f99a007b69766ffa52ab
XessiveObserver <xessiveobserver@gmail.com>
2023-06-24 10:48:11 +0300

    humburger menu & responsive layout added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const button = document.querySelector('#menu-button'); // Hamburger Icon
+const menu = document.querySelector('#menu'); // Menu
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
